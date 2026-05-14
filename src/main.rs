5155490af7b8a6911db280ef9eb9213b0c021e0d
Antoni Szymański <antoni.szymanski+github@proton.me>
2026-05-14 22:22:35 +0200

    feat: add --file option to get command

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,7 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Commands {
     /// Return a matching credential, if any exists.
-    Get,
+    Get { file: Option<PathBuf> },
     /// Store the credential.
     Store,
     /// Remove matching credentials, if any, from the storage.
@@ -52,14 +52,14 @@ enum Error {
 fn main() -> Result<(), Error> {
     let cli = Cli::parse();
     match cli.command {
-        Commands::Get => command_get(),
+        Commands::Get { file } => command_get(file),
         Commands::Store | Commands::Erase => Ok(()),
     }
 }
 
-fn command_get() -> Result<(), Error> {
+fn command_get(file: Option<PathBuf>) -> Result<(), Error> {
     let gc = GitCredential::from_reader(io::stdin()).context(ParseCredentialCtx)?;
-    let (mut file, path) = match open_credentials().context(OpenCredentialsCtx)? {
+    let (mut file, path) = match open_credentials(file).context(OpenCredentialsCtx)? {
         Some(v) => v,
         None => return Ok(()),
     };
@@ -78,20 +78,19 @@ fn command_get() -> Result<(), Error> {
         .map_or_else(|| Ok(()), |gc| gc.to_writer(io::stdout()).context(WriteCredentialCtx))
 }
 
-fn open_credentials() -> Result<Option<(File, PathBuf)>, io::Error> {
-    [
-        || env::var_os("GIT_CREDENTIALS").filter(|s| !s.is_empty()).map(PathBuf::from),
-        || dirs::config_dir().map(|p| p.join("git").join("credentials.json")),
-        || dirs::home_dir().map(|p| p.join(".git-credentials.json")),
-    ]
-    .into_iter()
-    .filter_map(|f| f())
-    .find_map(|path| match File::open(&path) {
-        Ok(file) => Some(Ok((file, path))),
-        Err(e) if e.kind() == io::ErrorKind::NotFound => None,
-        Err(e) => Some(Err(e)),
-    })
-    .transpose()
+fn open_credentials(file: Option<PathBuf>) -> Result<Option<(File, PathBuf)>, io::Error> {
+    use std::iter::{once, once_with};
+    once(file)
+        .chain(once_with(|| env::var_os("GIT_CREDENTIALS").filter(|s| !s.is_empty()).map(PathBuf::from)))
+        .chain(once_with(|| dirs::config_dir().map(|p| p.join("git").join("credentials.json"))))
+        .chain(once_with(|| dirs::home_dir().map(|p| p.join(".git-credentials.json"))))
+        .flatten()
+        .find_map(|path| match File::open(&path) {
+            Ok(file) => Some(Ok((file, path))),
+            Err(e) if e.kind() == io::ErrorKind::NotFound => None,
+            Err(e) => Some(Err(e)),
+        })
+        .transpose()
 }
 
 fn parse_credentials<'a>(input: &'a str, path: &Path) -> Result<Vec<Entry<'a>>, Error> {
